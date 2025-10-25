1608940c22e4734b71e20702b037642534950974
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-10-25 22:15:12 +0500

    Add project files.

---
+++ LeetCodeArray2/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int possibleindex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+            else if (nums[i] < target)
+            {
+                possibleindex = i;
+                continue;
+            }
+        }
+        return possibleindex;
+    }
+    private static void Main(string[] args)
+    {
+        int[] array = {1, 3, 5, 6 };
+        int result = SearchInsert(array, 5);
+        Console.WriteLine(result);
+    }
+}
